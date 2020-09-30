131412c8ebbc420667994804eedc71a7a9a1f43d
saogguim <gabriel.guimaraes@flex.com>
2020-09-30 14:38:41 -0300

    dictionary

---
+++ basics/dictionary.py
@@ -0,0 +1,40 @@
+# Dictionary: key-value variable
+# Only one value. If there is more than one, it consideres the last
+# You're able only to update values, not keys
+
+dict = {'name': 'John', 'age' : 25}
+
+print(dict['name'])
+print(dict['age'])
+print(dict)           # {'name': 'John', 'age': 25}
+print(dict.values())  # dict_values(['John', 25])
+
+dict['name'] = 'Rohn'
+print(dict['name'])
+
+dict['address'] = 'earth'
+print(dict)
+
+print(len(dict))
+dict1 = dict.copy()
+print(dict1)
+
+dict1['name'] = 'Mohit'
+
+print(dict.items())
+# dict_items([('name', 'Rohn'), ('age', 25), ('address', 'earth')])
+
+dict.update(dict1) # updates dict as dict1 values
+print(dict)
+
+dict2 = {'height': 160, 'weight': 60}
+dict.update(dict2)
+print(dict)
+#{'name': 'Mohit', 'age': 25, 'address': 'earth', 'height': 160, 'weight': 60}
+
+dict.clear()
+print(dict)  # {}
+
+del dict
+print(dict)
+
