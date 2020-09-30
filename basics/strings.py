cc9ab44567c26d00aa74ea8f49353cc65c469da7
saogguim <gabriel.guimaraes@flex.com>
2020-09-30 13:40:45 -0300

    New functions 4 strings

---
+++ basics/strings.py
@@ -0,0 +1,28 @@
+a = ' Hello Python '
+
+print(a)
+print(type(a)) # str
+
+print(a[0:4])  # prints from x to y char
+print(a[5:])   # prints from x to the end
+
+print(a*2)  # prints the string twice
+print(len(a)) 
+
+print(a.strip()) # equals to trim
+print(a.lstrip())
+print(a.rstrip())
+
+print(a.find("py")) # returns -1 if doesn't find
+print(a.find("Py")) 
+print(a.count("l")) # counts till first "l"
+
+print(a.replace("Python", "world"))
+print(a)
+
+print(a.split())
+print(a.split(" "))
+
+print(a.upper())
+print(a.lower())
+print(a.title())
