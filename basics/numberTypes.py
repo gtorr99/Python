0a62d37a8ba2355de7519476b642e8d2bd55b4e0
saogguim <gabriel.guimaraes@flex.com>
2020-09-30 11:53:34 -0300

    Added folder basics

---
+++ basics/numberTypes.py
@@ -0,0 +1,15 @@
+# Types of number values variables can receive
+
+i = 10
+f = 7.55
+c = 2 + 9j
+b = 0b101010110
+o = 0o1234567
+h = 0x2435A
+
+print(type(i)) # int
+print(type(f)) # float
+print(type(c)) # complex
+print(type(b)) # binary
+print(type(o)) # octal
+print(type(h)) # hexadecimal
---
+++ basics/test.py
@@ -0,0 +1,12 @@
+import keyword
+# this is a comment
+print("hi Gabriel. I'm learning the snake language, just like Harry Potter")
+
+x = 0
+y = "hello"
+
+if x == 0:
+    x = 1
+    print(x)
+
+print(keyword.kwlist)
