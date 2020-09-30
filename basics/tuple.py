95a75db6ff993cca513d29d31b0cd90db43d76e4
saogguim <gabriel.guimaraes@flex.com>
2020-09-30 13:52:01 -0300

    tuples

---
+++ basics/tuple.py
@@ -0,0 +1,16 @@
+tuple = ('first element', 3, 4.5)
+
+print(tuple)
+print(tuple[1:3])
+
+# tuple[0] = 'hello' -> can't update a tuple
+
+del tuple
+print(tuple)  # <class 'tuple'>
+
+tuple = (1.5, 3.1, 2.2)
+
+print(len(tuple))
+print(max(tuple))
+print(min(tuple))
+
