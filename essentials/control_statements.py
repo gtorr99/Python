08c1892282fc2f8bca57989ba425d4bb542a65c7
saogguim <gabriel.guimaraes@flex.com>
2020-10-01 16:31:31 -0300

    using pass and 'in range'

---
+++ essentials/control_statements.py
@@ -0,0 +1,18 @@
+# break
+# continue
+# pass
+
+# from 2 to 10, every 2 iterations, print
+for i in range(2, 10, 2):
+    if i == 6:
+        # break
+        continue
+    print(i)
+
+print('End')
+
+def my_function():
+    pass
+
+for val in range(1,5):
+    pass
