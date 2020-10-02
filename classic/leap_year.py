7ed4c6f7377d0a5a6ace3b08387e630a1497ee09
saogguim <gabriel.guimaraes@flex.com>
2020-10-02 11:14:36 -0300

    added comments

--- classic/leap_year.py
+++ classic/leap_year.py
@@ -1,5 +1,8 @@
 year = int(input("Enter a year: "))
 
+# First stat: checks if is a century leap year
+# Second stat: checks if the first is true or false
+# Third stat: checks if a non-century year is leap 
 if ((year % 400 == 0) if (year % 100 == 0) else (year % 4 == 0)):
     print(year, "is a leap year")
 else:
