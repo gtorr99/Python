4965986947008e381a29a6593a175c28e8282754
saogguim <gabriel.guimaraes@flex.com>
2020-10-05 09:52:03 -0300

    Improved factorial function

--- classic/factorial.py
+++ classic/factorial.py
@@ -1,8 +1,7 @@
 def factorial(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
 
-a = factorial(5)
-print(a)
+print(factorial(int(input("Enter the number: "))))
