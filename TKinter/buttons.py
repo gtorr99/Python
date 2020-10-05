05244e544db45645e9e157f6eab1cacfea528869
saogguim <gabriel.guimaraes@flex.com>
2020-10-05 16:48:09 -0300

    Changing buttons place

--- TKinter/buttons.py
+++ TKinter/buttons.py
@@ -2,12 +2,15 @@ from tkinter import *
 from functools import partial
 
 def bt_click(btn):
+    btn.place(x=200, y=150)
     print(btn["text"])
 
 screen = Tk()
 
+teste = 100
+
 bt1 = Button(screen, width=20, text="Botão 1")
-bt1.place(x=100, y=100)
+bt1.place(x=teste, y=100)
 bt1["command"] = partial(bt_click, bt1)
 
 bt2 = Button(screen, width=20, text="Botão 2")
