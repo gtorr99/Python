d628e46439f94d5b1123f701159eda48a04ab52f
saogguim <gabriel.guimaraes@flex.com>
2020-10-05 13:21:49 -0300

    Button

--- TKinter/place_layout_manager.py
+++ TKinter/place_layout_manager.py
@@ -1,11 +1,19 @@
 from tkinter import *
 
+def bt_click():
+    print("bt_click")
+    lb["text"] = "Funcionou!"
+
 screen = Tk()
 
 # Instance - Pattern Container
 lb = Label(screen, text="It's not a bug")
-lb.place(x=100, y=100)
+lb.place(x=100, y=150)
+
+bt = Button(screen, width=20, text="Ok", command=bt_click)
+bt.place(x=100, y=100)
+
+
 
 screen.geometry("300x300+200+200")
-
 screen.mainloop()
